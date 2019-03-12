37b93a36d2887fccc1df2fc92b4bb05c0589a310
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-03-12 09:46:22 -0700

    Implement authentication and added register routes

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ const express = require("express"),
     port = 3000,
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground = require("./models/campground"),
     Comment = require("./models/comment"),
+    User = require("./models/user"),
     seedDB = require("./seeds");
 
 seedDB();
+
+//Passport Configuration
+app.use(require("express-session")({
+    secret: "I love coding!!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(express.static(__dirname + "/public"))
 mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true });
@@ -105,5 +121,25 @@ app.post("/campgrounds/:id/comments", (req, res) => {
     // connect new comment to campground
     // redirect to campground show page
 })
+
+//AUTH Routes
+//show register form
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+//handle sign up logic
+app.post("/register", (req, res) => {
+    const newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/campgrounds");
+        });
+    });
+});
 
 app.listen(port, console.log(`YelpCamp is listening on ${port}`));
